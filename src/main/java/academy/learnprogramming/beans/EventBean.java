6b8872cd091e02c1e4840d477e02b50ac0091639
christofer sega <christofersega2@gmail.com>
2021-11-16 09:18:06 -0300

    first commit

--- src/main/java/academy/learnprogramming/beans/EventBean.java
+++ src/main/java/academy/learnprogramming/beans/EventBean.java
@@ -7,6 +7,7 @@ import academy.learnprogramming.events.EventData;
 import academy.learnprogramming.events.User;
 
 import javax.enterprise.event.Event;
+import javax.enterprise.inject.spi.Extension;
 import javax.inject.Inject;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -15,10 +16,15 @@ import java.util.concurrent.CompletionStage;
 @Web
 public class EventBean {
 
-    @Inject
-    private EventData eventData;
+
     @Inject
     private User user;
+
+    @Inject
+    Event<String> greetingEvent;
+
+    @Inject
+    Event<EventData> plainEvent;
 
     @Inject
     @PopularStand
@@ -28,15 +34,29 @@ public class EventBean {
     @Admin
     private Event<EventData> conditionalEvent;
 
+
+
     public void login() {
         //Do credentials checking to login in user then fire login event
         //someSecurityManager.loginUser(user.getEmail, user.getPassword)
 
+        greetingEvent.fire("Hello from priority");
+
+        plainEvent.fire(new EventData(user.getEmail(), LocalDateTime.now()));
+
+
+
+
         LocalDateTime now = LocalDateTime.now();
         System.out.println(now);
 
-//        eventDataEvent.fire(new EventData(user.getEmail(), LocalDateTime.now()));
+        eventDataEvent.fire(new EventData(user.getEmail(), LocalDateTime.now()));
+
+
         CompletionStage<EventData> fireAsync = eventDataEvent.fireAsync(new EventData(user.getEmail(), LocalDateTime.now()));
+
+
+
 
         long secs = ChronoUnit.SECONDS.between(now, LocalDateTime.now());
 
---
+++ src/main/java/academy/learnprogramming/events/EventPriority.java
@@ -0,0 +1,28 @@
+package academy.learnprogramming.events;
+
+import javax.annotation.Priority;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.interceptor.Interceptor;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RequestScoped
+public class EventPriority {
+    //@Priority(Interceptor.Priority.APPLICATION + 200)
+    //@Priority(Interceptor.Priority.APPLICATION) not working
+    @Inject
+    Logger logger;
+
+    void greetingReceiver1(@Observes String greeting) {
+        logger.log(Level.INFO, "Greeting 1 with lower priority invoked with message " + greeting + "1");
+
+    }
+
+    //Higher priority
+    void greetingReceiver2(@Observes String greeting) {
+        logger.log(Level.INFO, "Greeting 2 with higher priority invoked with message " + greeting + "2");
+    }
+}
